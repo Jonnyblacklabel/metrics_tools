87634e22c49aaf0c3b767c94126248e640efc1cd
Johannes Kunze <johannes.kunze@gettraction>
2018-05-29 10:34:20 +0200

    changed versioning
    changed versioning for upload to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="metrics_tools",
-    version="0.0.1",
+    version="0.0.2",
     author="Jonnyblacklabel",
     author_email="mail@jonnyblacklabel.de",
     description="Package for making calls to metrics.tools API",
